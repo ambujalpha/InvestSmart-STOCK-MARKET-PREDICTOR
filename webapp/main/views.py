f53ca7a1a85af54d0dff6436646b3003309fe51d
ambujalpha <jainambuj8@gmail.com>
2020-05-13 20:52:37 +0530

    dynamic_stocks

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from main.models import Main
 
 
 def home(request):
 
-    return render(request, 'front/home.html')
+    site = Main.objects.get(pk=2)
+    return render(request, 'front/home.html', {'site': site})
+
--- webapp/stock/views.py
+++ webapp/stock/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Stock
+from main.models import Main
 
 
 def stock(request):
 
-    return render(request, 'front/stock.html')
+    site = Main.objects.get(pk=2)
+    allstocks = Stock.objects.all()
+    return render(request, 'front/stock.html', {'site': site, 'allstocks': allstocks})
 
 
 def stock_detail(request, word):
 
-    return render(request, 'front/stock_detail.html')
+    site = Main.objects.get(pk=2)
+    showstock = Stock.objects.filter(name=word)
+    return render(request, 'front/stock_detail.html', {'site': site, 'showstock': showstock})
