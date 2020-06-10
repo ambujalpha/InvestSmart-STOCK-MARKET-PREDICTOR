908b3dc69b475f09420a7048d01f61f74cbeff85
ambujalpha <jainambuj8@gmail.com>
2020-06-10 11:32:11 +0530

    services_pages

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -6,4 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^$', views.home, name='home'),
+    url(r'^consult/$', views.consult, name='consult'),
+    url(r'^management/$', views.management, name='management'),
+    url(r'^market/$', views.market, name='market'),
+    url(r'^monitoring/$', views.monitoring, name='monitoring'),
+    url(r'^investment/$', views.investment, name='investment'),
 ]
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -7,3 +7,27 @@ def home(request):
     site = Main.objects.get(pk=2)
     return render(request, 'front/home.html', {'site': site})
 
+
+def consult(request):
+
+    return render(request, 'front/consult.html')
+
+
+def market(request):
+
+    return render(request, 'front/market.html')
+
+
+def monitoring(request):
+
+    return render(request, 'front/monitoring.html')
+
+
+def investment(request):
+
+    return render(request, 'front/investment.html')
+
+
+def management(request):
+
+    return render(request, 'front/management.html')
