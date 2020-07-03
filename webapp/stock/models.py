f952c6942306d969f3b259b2f5f263b239fc42d6
ambujalpha <jainambuj8@gmail.com>
2020-07-03 09:41:12 +0530

    model_stock_updated

--- webapp/stock/models.py
+++ webapp/stock/models.py
@@ -6,8 +6,6 @@ class Stock(models.Model):
 
     name = models.CharField(max_length=30)
     txt = models.TextField(default="-")
-    invest = models.CharField(max_length=15,default="-")
-    divest = models.CharField(max_length=15,default="-")
     picname = models.TextField(default="-")
     picurl = models.TextField(default="-")
 
